fcbb8f7882fd03bf727f810ee70a7dd78e0acdc9
kob555 <chom.kappa@gmail.com>
2021-02-05 16:33:56 +0700

    Finished

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -32,3 +32,38 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a,int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a+i);
+        if((i+1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = double(rand() % 101)/ 100;
+    }
+}
+
+void findRowSum(const double *a,double *b,int N,int M){
+    
+    for(int i=0; i<N; i++){
+    b[i] = 0;
+        for(int j=0; j<M; j++){
+           b[i] += *((i*M)+j+a);
+           }
+    }
+    }
+
+void findColSum(const double *a,double *b,int N,int M){
+    
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+           if(i==0) b[j] = 0;
+           b[j] += *a++;
+           }
+    }
+    }
